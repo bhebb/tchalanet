4c083dc9bbd27a421e3b69f19570829bf13fc7a4
Stevens Nelson <steevens.nelson@gmail.com>
2026-01-23 16:09:40 -0500

    fet(server) refactor tchalanet server

--- tchalanet-server/src/main/java/com/tchalanet/server/catalog/address/application/port/out/AddressReaderPort.java
+++
@@ -1,9 +0,0 @@
-package com.tchalanet.server.catalog.address.application.port.out;
-
-import com.tchalanet.server.catalog.address.domain.model.Address;
-import java.util.Optional;
-import java.util.UUID;
-
-public interface AddressReaderPort {
-  Optional<Address> findById(UUID id);
-}
---
+++ tchalanet-server/src/main/java/com/tchalanet/server/catalog/address/internal/cache/AddressCacheNames.java
@@ -0,0 +1,8 @@
+package com.tchalanet.server.catalog.address.cache;
+
+public final class AddressCacheNames {
+  private AddressCacheNames() {}
+
+  public static final String ACTIVE = "catalog:address:active";
+  public static final String BY_ID = "catalog:address:id";
+}
