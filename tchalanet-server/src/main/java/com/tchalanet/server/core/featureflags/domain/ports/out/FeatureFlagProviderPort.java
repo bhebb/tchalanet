7f8d242b09929791a99e0b3d5e681fe9a97b01ca
Stevens Nelson <steevens.nelson@gmail.com>
2026-05-12 11:10:10 -0400

    chore(bus): refactoring

--- tchalanet-server/src/main/java/com/tchalanet/server/core/featureflags/domain/ports/out/FeatureFlagProviderPort.java
+++
@@ -1,8 +0,0 @@
-package com.tchalanet.server.core.featureflags.domain.ports.out;
-
-import com.tchalanet.server.core.featureflags.domain.model.FeatureContext;
-
-/** Outbound Port for integrating with a feature flag provider (e.g., Unleash, LaunchDarkly). */
-public interface FeatureFlagProviderPort {
-  boolean isEnabled(String flagKey, FeatureContext context);
-}
